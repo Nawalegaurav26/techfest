04663c3acd2e716ec8b03c9eb3191716bf3046e1
Nawalegaurav26 <nawalegaurav26@gmail.com>
2026-06-13 23:09:36 +0530

    refactor(workshops): clean up workshop registration flow details

--- src/pages/Workshops.jsx
+++ src/pages/Workshops.jsx
@@ -1,3 +1,4 @@
+/* Techfest 2026 - Telemetry Log 14 */
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { soundEffects } from '../utils/soundEffects';
